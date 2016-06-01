b02b2227dd4986fd6dc96f5629d20f5a5b4f74e0
cmertenscm <c.mertens@accenture.com>
2016-06-01 17:17:38 -0500

    Remove default route, add static middleware

--- src/server/boot/root.js
+++ src/server/boot/root.js
@@ -1,6 +1,2 @@
 module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
 };
