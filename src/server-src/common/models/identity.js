1fb979193e3b3c97c7507ebb54f8fe35b4849502
cmertenscm <c.mertens@accenture.com>
2016-06-06 13:16:27 -0500

    Custom registration func & triggers

--- src/server-src/common/models/identity.js
+++ src/server-src/common/models/identity.js
@@ -1,3 +1,47 @@
-module.exports = function(Identity) {
+var Faker = require('faker');
 
+module.exports = function(Identity) {
+  Identity.register = function(email, pass, firstName, lastName, caseId, cb){
+    Identity.create({email:email, password:pass}, function (err, identityInstance){
+      if(err !== null){
+        console.log("User creation failed with error:");
+        console.log(err);
+        return cb('User creation failed');
+      }
+      console.log("User created for email:" + email + ", case " + caseId);
+      identityInstance.FirstName = firstName;
+      identityInstance.LastName = lastName;
+      identityInstance.CurrentCaseNumber = caseId;
+      identityInstance.save();
+      return cb(null, 'ok', "application/json");
+    });
+  };
+  Identity.remoteMethod(
+    'register',
+    { 
+      http : {path:'/register',verb:'post'},
+      accepts :[
+        {arg: 'email', type:'string'},
+        {arg: 'pass', type:'string'},
+        {arg: 'firstName', type:'string'},
+        {arg: 'lastName', type:'string'},
+        {arg: 'caseId', type:'string'}
+      ],
+      returns : {arg:'status', type:'string'}
+    }
+  );
+
+  Identity.afterRemote('register', function(ctx, IdentityInstance, next){
+    console.log("Mocking case data...");
+    // create new case
+
+    // attach a child 
+
+
+    // attach a social worker
+
+    if(next && typeof next === 'function'){
+      next();
+    }
+  });
 };
--- src/server-src/common/models/postal-address.js
+++ src/server-src/common/models/postal-address.js
@@ -1,3 +1,5 @@
 module.exports = function(PostalAddress) {
-
+  PostalAddress.afterRemote('create', function(ctx, PostalAddressInstance, next){
+    var GeocodedPoint = PostalAddress.app.models.GeocodedPoint;
+  });
 };
