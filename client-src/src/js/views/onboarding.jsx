0ada55a0de1b6d767b1ce4afc665fb085c113742
Matt McCloskey <matt.mccloskey@accenture.com>
2016-06-08 17:52:11 -0500

    Make onboarding footer match ffas footer

--- client-src/src/js/views/onboarding.jsx
+++ client-src/src/js/views/onboarding.jsx
@@ -99,13 +99,15 @@ module.exports = React.createClass({
 						title="Daily Routines" 
 						body={!this.state.dependent.FirstName ? '' : 'What are some of ' + this.state.dependent.FirstName + ' routines?'} 
 						items={this.getOptions('dailyRoutines')} />
-					<p style={{marginTop: '2em', marginBottom: '2em'}}>
+				</div>
+				<div className="footer">
+					<div className="container text-right">
 						{
 							this.allOptionsSelected()
 								? <Link to={this.state.mode === 'setup' ? '/ffa' : '/dashboard'} className="btn btn-primary">{this.state.mode === 'setup' ? 'Next' : 'Update'}</Link>
-								: <p>Please make your selections above before continuing</p>
+								: <p className="text-center">Please make your selections above before continuing</p>
 						}
-					</p>
+					</div>
 				</div>
 			</div>
 		)
