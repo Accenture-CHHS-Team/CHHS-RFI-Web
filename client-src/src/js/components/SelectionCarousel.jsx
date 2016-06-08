d91055840b2ffeed5144cde466046995c3fe0cec
Matt McCloskey <matt.mccloskey@accenture.com>
2016-06-08 18:17:24 -0500

    Change carousels to 2 items on small break point

--- client-src/src/js/components/SelectionCarousel.jsx
+++ client-src/src/js/components/SelectionCarousel.jsx
@@ -30,7 +30,16 @@ module.exports = React.createClass({
 			infinite: false,
 			speed: 500,
 			slidesToShow: 3,
-			slidesToScroll: 1
+			slidesToScroll: 1,
+			responsive: [
+				{
+					breakpoint: 768,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2
+					}
+				}
+			]
 		};
 		return (
 			<div className="selection-carousel text-left">
